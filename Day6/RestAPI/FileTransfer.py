602a753a762c9a9f24690f74e31b0f91e9f1a74f
Subraja <92440544+subrajar@users.noreply.github.com>
2022-04-15 14:49:06 +0530

    Create FileTransfer.py

---
+++ Day6/RestAPI/FileTransfer.py
@@ -0,0 +1,51 @@
+import os
+from flask import Flask, request, jsonify
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'C:/uploads'
+
+app = Flask(__name__)
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/file-upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        resp = jsonify({'message': 'No file part in the request'})
+        resp.status_code = 400
+        return resp
+    file = request.files['file']
+    if file.filename == '':
+        resp = jsonify({'message': 'No file selected for uploading'})
+        resp.status_code = 400
+        return resp
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        resp = jsonify({'message': 'File successfully uploaded'})
+        resp.status_code = 201
+        return resp
+    else:
+        resp = jsonify({'message': 'Allowed file types are txt, pdf, png, jpg, jpeg, gif'})
+        resp.status_code = 400
+        return resp
+
+
+@app.route('/file-read/<file_name>')
+def read_file(file_name):
+    if allowed_file(file_name):
+        filename=os.path.join(app.config['UPLOAD_FOLDER'],file_name)
+        file = open(filename, 'r')
+        return file.read()
+
+
+if __name__ == "__main__":
+    app.run()
