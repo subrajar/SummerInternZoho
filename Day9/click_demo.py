182da8f9f53e0ce919328f32c663f21aafb94c02
Subraja <92440544+subrajar@users.noreply.github.com>
2022-04-19 17:42:12 +0530

    Update click_demo.py

--- Day9/click_demo.py
+++ Day9/click_demo.py
@@ -3,7 +3,7 @@ import click
 @click.command()
 @click.option('--string', default ='World', help ='This is a greeting')
 def hello(string):
-	click.echo("Hello, {}".format(string))
+	click.echo(f"Hello, {string}")
 
 if __name__=="__main__":
   hello()	
